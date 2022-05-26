761eb3472c6e5662023749f4cdfa7737b8bffc20
Richard <richwats@cisco.com>
2022-05-26 12:38:53 +1000

    test with oauth

--- main.tf
+++ main.tf
@@ -61,6 +61,10 @@ provider "thousandeyes" {
   token = var.te_token # Passed from Workspace Variable
 }
 
+data "thousandeyes_agent" "test" {
+  agent_name = "Sydney, Australia"
+}
+
 module "fso" {
   source = "github.com/cisco-apjc-cloud-se/terraform-helm-fso"
 

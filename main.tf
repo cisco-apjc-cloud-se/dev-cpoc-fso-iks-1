04bbd4df55bb783a6b1f8469ddb1acba4b4ee025
Richard <richwats@cisco.com>
2022-05-26 15:35:22 +1000

    retry with new provider

--- main.tf
+++ main.tf
@@ -61,15 +61,11 @@ provider "thousandeyes" {
   token = var.te_token # Passed from Workspace Variable
 }
 
-# data "thousandeyes_agent" "test" {
-#   agent_name = "Sydney, Australia"
-# }
-
 module "fso" {
   source = "github.com/cisco-apjc-cloud-se/terraform-helm-fso"
 
   thousandeyes = {
-    enabled = false
+    enabled = true
     http_tests = {
       fso-demo-app = {
         name                    = "fso-demo-app"
